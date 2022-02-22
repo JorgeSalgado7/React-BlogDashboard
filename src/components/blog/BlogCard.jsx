7f17206b86f8f0cb5a252bf9e3894d6fdfeb670e
MariaMeza03 <mmeza@licorne.mx>
2022-02-22 08:09:15 -0600

    Modificaciones

--- src/components/blog/BlogCard.jsx
+++ src/components/blog/BlogCard.jsx
@@ -1,23 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+const Pulse = () => {
+  
+    return(
+        <div className="blog-card">
+           <button><e className='pi pi-pencil'> Editar</e></button>
+           <br></br>
+           <button><r className='pi pi-trash'> Eliminar</r></button>
+        </div>
+    
+    )
+}
+
 
 const BlogCard = () => {
+    
+    const [pulse,setPulse] = useState(false);
 
     return(
+        <div className ="box">
+            
+           <div className="imgBx">
+             <img src="" alt=""/>
+             
 
-        <div className ="blog-card">
+                 <div className= "Ellipse">
+                         <button onClick={()=> setPulse(!pulse)} onClick={()=> setPulse(!pulse)} className="pi pi-ellipsis-v">
+                         </button> 
+                         
+                 </div>
+                 {pulse && <Pulse />}
 
-            <div className="imgBx">
-                <img src="" alt=""/>
-            </div>
-
-            <div className="content">
+           </div>
+            
+             <div className="content">
                 <h1>Título</h1>
                 <p>Autor:</p>
                 <p>Fecha de publicacion:</p>
             </div>
 
         </div>
-         
+        
     )
 }
 
